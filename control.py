a1f43edaa84129449e28578b0c7f044ab2602e8a
Krish-Jain-IITJ <jainkrish283@gmail.com>
2026-02-23 14:55:04 +0000

    hi

---
+++ control.py
@@ -0,0 +1,9 @@
+#i am devising loitering control for this
+import math
+r = 1000
+theta =[]
+theta_b = 0.0 , theta_a = 0.0
+lembda_a = 0.0 , lembda_b = 0.0
+r_cmd = 5
+a = pow(math.sin((theta_b-theta_a)/2),2) + math.cos(theta_b)*math.cos_a(theta_a)*pow(math.sin(lembda2-lembda1)/2 , 2)
+d = 2*r*math.asin(math.sqrt(a))
